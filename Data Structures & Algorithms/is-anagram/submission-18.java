321d403b55465167009d90bb1a3369a53478a168
Vi Nguyen <126067105+DuvNguyen@users.noreply.github.com>
2026-09-14 16:28:58 +0700

    Add: is-anagram - submission-18

---
+++ Data Structures & Algorithms/is-anagram/submission-18.java
@@ -0,0 +1,43 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] sArray = s.toCharArray();
+        Arrays.sort(sArray);
+
+        char[] tArray = t.toCharArray();
+        Arrays.sort(tArray);
+
+        int countSame = 0;
+
+        if (!sameLength(sArray, tArray)) {
+            return false;
+        }
+
+        for (int i = 0; i < sArray.length; i++) {
+            if (sArray[i] == tArray[i]) {
+                countSame++;
+            }
+            if (countSame == sArray.length){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean sameLength(char[] array1, char[] array2){
+        if (array1.length > array2.length || array1.length < array2.length) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
+
+/*
+- String -> to char array
+- sort aphabetically
+- check if samelength -> consider ana
+    if not: not ana
+- for (same -> count++):
+    = count -> ana
+    != count -> not ana
+*/
